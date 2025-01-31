d582ea17bfb4d1c5d7e38437b6a49e91de029aad
Elias Ibañez <elias-ibanez@outlook.es>
2025-01-30 20:33:35 -0500

    se agregan los nombres de los amigos por medio del input y se muestran en una lista

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+const agregarAmigo = () => {
+    let nombre = document.getElementById('amigo').value;
+    if(nombre == ''){
+        alert('Debe ingresar un nombre');
+        return;
+    }
+    amigos.push(nombre);
+    mostrarAmigos();
+    document.getElementById('amigo').value = '';
+    console.log(nombre);
+}
+
+const mostrarAmigos = () => {
+    let amigosList = document.getElementById('listaAmigos');
+    amigosList.innerHTML = '';
+    amigos.forEach(amigo => {
+        amigosList.innerHTML += `<li>${amigo}</li>`;
+    });
+}
