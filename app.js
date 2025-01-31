2393a6e705669e09332302af78ceb86bb4f7c511
Elias Ibañez <elias-ibanez@outlook.es>
2025-01-30 21:19:52 -0500

    se agrego un snackbar para mostrar los mensajes al usuario

--- app.js
+++ app.js
@@ -2,21 +2,36 @@
 let amigos = [];
 
 const agregarAmigo = () => {
-    let nombre = document.getElementById('amigo').value;
-    if(nombre == ''){
-        alert('Debe ingresar un nombre');
-        return;
-    }
-    amigos.push(nombre);
-    mostrarAmigos();
-    document.getElementById('amigo').value = '';
-    console.log(nombre);
-}
+  let nombre = document.getElementById("amigo").value;
+  if (nombre == "") {
+    // alert('Debe ingresar un nombre');
+    snackbar("Debe ingresar un nombre");
+    return;
+  }
+  amigos.push(nombre);
+  mostrarAmigos();
+  document.getElementById("amigo").value = "";
+  console.log(nombre);
+};
 
 const mostrarAmigos = () => {
-    let amigosList = document.getElementById('listaAmigos');
-    amigosList.innerHTML = '';
-    amigos.forEach(amigo => {
-        amigosList.innerHTML += `<li>${amigo}</li>`;
-    });
-}
+  let amigosList = document.getElementById("listaAmigos");
+  amigosList.innerHTML = "";
+  amigos.forEach((amigo) => {
+    amigosList.innerHTML += `<li>${amigo}</li>`;
+  });
+};
+
+const sortearAmigo = () => {
+  let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+  document.getElementById("resultado").innerHTML = amigoSorteado;
+};
+
+const snackbar = (mensaje) => {
+  let snackbarElement = document.getElementById("snackbar");
+  snackbarElement.className = "show";
+  snackbarElement.innerHTML = mensaje;
+  setTimeout(function () {
+    snackbarElement.className = snackbarElement.className.replace("show", "");
+  }, 3000);
+};
